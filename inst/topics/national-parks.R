e7a88b6572aa9927dc12fbee815caf816c8678b5
Molly Karnes <mkarnes@ucmerced.edu>
2019-03-06 12:49:22 -0800

    adding national-parks topic list for bingo card

---
+++ inst/topics/national-parks.R
@@ -0,0 +1,29 @@
+## Aim : Bingo card of National Parks visited
+## Author : Molly Karnes
+
+c(
+  "Arches",
+  "Badlands",
+  "Carlsbad Caverns",
+  "Mammoth Cave",
+  "Yosemite",
+  "Petrified Forest",
+  "Death Valley",
+  "Everglades",
+  "Grand Canyon",
+  "Great Smokey Mountains",
+  "Mesa Verde",
+  "Rocky Mountain",
+  "Sequoia",
+  "Shenandoah",
+  "Yellowstone",
+  "Zion",
+  "Big Bend",
+  "Bryce Canyon",
+  "Denali",
+  "Glacier",
+  "Grand Canyon",
+  "Hawaii Volcanoes",
+  "Indiana Dunes",
+  "Mount Rainier"
+)
